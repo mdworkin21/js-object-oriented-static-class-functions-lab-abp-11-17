1a49def3b262dac96b45e7cbd86a3f54c598aff4
Matthew Dworkin <mdworkin21@gmail.com>
2017-11-14 22:07:48 +0000

    automatically backed up by learn

--- question.js
+++ question.js
@@ -3,9 +3,13 @@
 class Question{
 
   constructor(){
-    this.constructor._All.push(this)
+    this.save()
   }
 
+
+  save(){
+    this.constructor._All.push(this)
+  }
   static All(){
       return this._All
   }
