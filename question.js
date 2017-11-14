060df4f6d4c08e21d14c3e021cce679e06bbd06c
Matthew Dworkin <mdworkin21@gmail.com>
2017-11-14 22:43:01 +0000

    Done.

--- question.js
+++ question.js
@@ -1,1 +1,28 @@
 'use strict';
+
+class Question{
+
+  constructor(content, id){
+    this.content = content
+    this.id = id
+    this.save()
+  }
+
+
+  save(){
+    this.constructor._All.push(this)
+  }
+
+  static All(){
+      return this._All
+  }
+
+  static Find(id){
+    return this.All()[id-1]
+
+  }}
+
+
+
+
+Question._All = []
